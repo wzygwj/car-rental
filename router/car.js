f05078372ad1a9e8ea09ba4efe3de791888f2f24
wangzeyu <172546405@qq.com>
2017-05-01 17:15:01 +0800

    feature:联系我们;详情页面

--- router/car.js
+++ router/car.js
@@ -2,18 +2,29 @@ const express = require('express')
 const db = require('../mongoose')
 const router = express.Router();
 
-router.get('/slider', (req, res) => {
-    db.Car.find().limit(5).exec((err, data) => {
-        res.render('rent/slider', {
+// 获取车辆信息，page：跳转的页面，size：获取数量
+router.get('/list/:page/:size', (req, res) => {
+    db.Car.find().limit(parseInt(req.params.size)).exec((err, data) => {
+        res.render('rent/' + req.params.page, {
             cars: data
         });
     })
 })
-router.get('/hot', (req, res) => {
-    db.Car.find().limit(6).exec((err, data) => {
-        res.render('rent/hot', {
-            cars: data
-        });
+
+// 车辆详情页面 
+router.get('/one/:id', (req, res) => {
+    db.Car.findById(req.params.id, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('rent/one', { car: data })
+        }
     })
 })
+
+// 联系我们
+router.get('/contact', (req, res) => {
+    res.render('rent/contact')
+})
+
 module.exports = router;
